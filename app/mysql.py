063b55dffeef23da3977fd4af879316771ac19e6
Ernesto Mello <emello969@gmail.com>
2022-03-18 17:22:15 -0300

    cambios para que funcione con la autenticacion básica y con token

--- app/mysql.py
+++ app/mysql.py
@@ -9,4 +9,5 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'pao2930'
 app.config['MYSQL_DATABASE_DB'] = 'siet_fe'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
 mysql.init_app(app)
--- app/routes.py
+++ app/routes.py
@@ -2,26 +2,52 @@ from app import app
 from crypt import methods
 from operator import methodcaller
 from flask import Flask, jsonify,request,flash
-from flaskext.mysql import MySQL
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from app.mysql import mysql
+import base64
+import os
 
 
-basic_auth = HTTPBasicAuth
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 
-#@basic_auth.verify_password
-#def verify_password(username, password):
-#    conn = mysql.connect()
-#    cur = conn.cursor()
-#    cur.execute('select * from socio')
-#    user = cur.fetchall()
+@basic_auth.verify_password
+def verify_password(username, password):
+    #conn = mysql.connect()
+    #cur = conn.cursor()
+    #cur.execute('select * from socio')
+    #user = cur.fetchall()
+    user = {
+        "usuario":username,
+        "password": password
+    }
+    if username == "emello" and password == "pao2930":
+        return True
+    return False
+    #return print(user)
+
+@token_auth.verify_token
+def verify_token(token):
+    if token == "lolita la loca":
+        return True
+    return False 
+
+@app.route('/token', methods =['GET'])
+@basic_auth.login_required
+def get_token(expires_in=3600):
+    #now = datetime.utcnow()
+    #cur = mysql.connect().cursor()
+    #cur.execute('select * from socio')
+    #rows = cur.fetchall()
+    token = base64.b64encode(os.urandom(24)).decode('utf-8')
+    #token_expiration = now + timedelta(seconds=expires_in)
+    resp = jsonify(token)
+    resp.status_code = 200
+    return "lolita la loca"#resp
     
 
-
-
-
-
 @app.route('/users', methods =['GET'])
+@token_auth.login_required
 def get_users():
     #try:
         conn = mysql.connect()
@@ -39,6 +65,7 @@ def get_users():
     #    conn.close()
 
 @app.route('/users/<id>', methods = ['GET'])
+@token_auth.login_required
 def get_user(id):
     conn = mysql.connect()
     cur = conn.cursor()
@@ -50,5 +77,3 @@ def get_user(id):
     #print('aca traigo uno solo')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
